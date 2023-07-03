ef49d6bb34fe003849a03c09553ae5bebcba25c3
Sanket Wagh <sanketwagh147@gmail.com>
2023-07-04 00:12:28 +0530

    ba ckup

--- my_website/accounts/signals.py
+++ my_website/accounts/signals.py
@@ -5,17 +5,17 @@ from .models import User, UserProfile
 
 
 # post_save.connect(post_create_profile_receiver, sender=User)
-@receiver(post_save, sender=User)
-def post_save_create_profile_receiver(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
 
-    else:
-        try:
-            profile = UserProfile.objects.get(user=instance)
-            profile.save()
-        except Exception:
-            UserProfile.objects.create(user=instance)
+#     else:
+#         try:
+#             profile = UserProfile.objects.get(user=instance)
+#             profile.save()
+#         except Exception:
+#             UserProfile.objects.create(user=instance)
 
 
 @receiver(pre_save, sender=User)
