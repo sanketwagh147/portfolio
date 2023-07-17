61f1fb0208af5da41e508342cd98fca2029bd00b
Sanket Wagh <sanketwagh147@gmail.com>
2023-07-18 01:15:21 +0530

    started working on cv

--- my_website/home/urls.py
+++ my_website/home/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="home")]
+urlpatterns = [path("", views.index, name="home"),
+               path("resume", views.resume, name="resume")]
--- my_website/home/views.py
+++ my_website/home/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, "home/index.html")
     # return "Work in progress"
+
+def resume(request):
+    return render(request, "home/resume.html")
